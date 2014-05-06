f00efa1ab256907cf9d63fe67fcdc1237a1856c0
jhiltscher42 <j.hiltscher@gmail.com>
2014-05-05 22:08:08 -0500

    debugging reventing iPad bouncing

--- jscript/main.js
+++ jscript/main.js
@@ -3,6 +3,7 @@ require(["camera"],function(camera){
 		var myCamera=$("#viewCam").data("camera");
 		myCamera.setModel({draw:function(gc,extants){}});
 		myCamera.addHandler(['touchmove'],function(coords,touchEvt){
+			alert(JSON.stringify(touchEvt));
 			touchEvt.preventDefault();
 		    });
 	    });
